cc6727bb46ea4bb8f472b7cd19e08d761d327cf0
padawan <padawan327@gmail.com>
2026-01-26 16:39:39 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+    This is a small home-grown project for practicing C++. 
+    It's a small console game about a knight who must ascend a hundred-level tower, 
+    overcoming obstacles and defeating enemies and bosses along the way.
+
+    VERSION 0.0.0
+*/
+
+#include <iostream>
+
+int main() {
+
+    for (int i = 0; i < 10; i++) {
+        std::cout << "Hello, World!" << std::endl;
+    }
+
+    return -0;
+}
