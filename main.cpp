df2f622f216fa95a580b746e23bd9f3406b3d2f7
padawan <padawan327@gmail.com>
2026-02-25 21:15:46 +0300

    Creation of the first version of the combat system.

--- main.cpp
+++ main.cpp
@@ -10,19 +10,68 @@
 #include "game.h"
 #include "character.h"
 #include "storyTeller.h"
+#include <random>
+#include <windows.h>
 
 
 int main() {
 
-    setlocale(LC_ALL, "");
+    // Устанавливаем кодировку UTF-8 для консоли
+    SetConsoleOutputCP(CP_UTF8);
 
-    //Character biba{ "Biba", 135, 5, 35};
-    //Character boba{ "Boba", 70, 40, 10};
-
-    //boba.giveDamage(biba);
-    //std::cout << "HP Boba: " << biba.getHealth() << std::endl;
+    // Опционально: для ввода тоже
+    SetConsoleCP(CP_UTF8);
 
     StoryTeller::printPoster();
+
+    Character biba{ "Biba", 135, 5, 35};
+    Character boba{ "Boba", 70, 40, 10};
+
+    while (biba.isLive() && boba.isLive())
+    {
+        //system("cls");
+        
+        std::cout << "HP Героя: " << biba.getHealth() << std::endl;
+        std::cout << "HP противника: " << boba.getHealth() << std::endl;
+        
+        int choice_attack;
+        int choice_protect;
+
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> distrib(1, 3);
+        int random_atack = distrib(gen);
+        int random_protect = distrib(gen);
+
+        std::cout << "Выберите куда ударить: \n 1) Голова \n 2) Тело \n 3) Рука \n" << std::endl;
+        std::cin>> choice_attack;
+
+        if (choice_attack != random_protect)
+        {
+            biba.giveDamage(boba);
+            std::cout << "Атака прошла успешно \n" << std::endl;
+        }else{
+            std::cout << "Атака была заблокирована \n" << std::endl;
+        }
+
+        //std::cout << "HP Героя: " << biba.getHealth() << std::endl;
+        //std::cout << "HP противника: " << boba.getHealth() << std::endl;
+
+        std::cout << "Выберите что защитить: \n 1) Голова \n 2) Тело \n 3) Рука \n" << std::endl;
+        std::cin >> choice_protect;
+
+        if (choice_protect != random_atack)
+        {
+            boba.giveDamage(biba);
+            std::cout << "Атака противника прошла успешно \n" << std::endl;
+        }
+        else {
+            std::cout << "Атака противника была заблокирована \n" << std::endl;
+        }
+
+        //std::cout << "HP Героя: " << biba.getHealth() << std::endl;
+        //std::cout << "HP противника: " << boba.getHealth() << std::endl;
+    }
 
     return 0;
 }
--- storyTeller.cpp
+++ storyTeller.cpp
@@ -2,17 +2,28 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <conio.h>
+#include <windows.h>
 
 StoryTeller::StoryTeller() {}
 
 StoryTeller::~StoryTeller() {}
 
 void StoryTeller::printPoster() {
+
+    HWND hwnd = GetConsoleWindow();
+
+    ShowWindow(hwnd, SW_MAXIMIZE);
+
     std::ifstream file("poster.txt");
     std::string line;
     while (std::getline(file, line)) {
         std::cout << line << '\n';
     }
+
+    std::cout << "Press any key ... " << '\n';
+    char key = _getch();
+    system("cls");
 }
 
 
